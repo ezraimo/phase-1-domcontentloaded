116fd1a92e0e12535031e39e964ee8809916c239
EZRA KIPRUTO SEREM <ezraserem4038@gmail.com>
2025-03-23 13:14:15 +0300

    domcontentloaded

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+    const paragraph = document.querySelector("p");
+    if (paragraph) {
+      paragraph.textContent = "This is really cool!";
+    }
+  });
+  
